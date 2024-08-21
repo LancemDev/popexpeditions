657ba086d4b533328f7f48de6cb5619731b05c43
LancemDev <munyaolance1@gmail.com>
2024-08-21 14:22:02 +0300

    Xwqw

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="light">
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,45 +12,38 @@
     <link href="https://fonts.googleapis.com/css2?family=Paytone+One&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap">
-
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-  
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/css/tw-elements.min.css" />
 
-    <!-- Alphine  -->
+    <!-- Alpine.js -->
     <script src="//unpkg.com/alpinejs" defer></script>
     <script src="https://cdn.tailwindcss.com/3.3.0"></script>
     <script>
     tailwind.config = {
         darkMode: "class",
         theme: {
-        fontFamily: {
-            sans: ["Roboto", "sans-serif"],
-            body: ["Roboto", "sans-serif"],
-            mono: ["ui-monospace", "monospace"],
-        },
+            fontFamily: {
+                sans: ["Roboto", "sans-serif"],
+                body: ["Roboto", "sans-serif"],
+                mono: ["ui-monospace", "monospace"],
+            },
         },
         corePlugins: {
-        preflight: false,
+            preflight: false,
         },
     };
     </script>
-
 </head>
+<body>
+    {{ $slot }}
 
-    <body>
-        {{ $slot }}
-
-        <!-- Footer -->
-
-        <section class="footer">
+    <!-- Footer -->
+    <section class="footer">
         <div class="foot">
             <div class="footer-content">
-                
                 <div class="footlinks-1">
                     <div class="column">
                         <h4>Quick Links</h4>
@@ -64,7 +55,6 @@
                             <li><a href="/contact-us">Contact Us</a></li>
                         </ul>
                     </div>
-
                     <div class="column">
                         <h4>Support</h4>
                         <ul>
@@ -75,7 +65,6 @@
                         </ul>
                     </div>
                 </div>
-
                 <div class="footlinks">
                     <h4>Connect</h4>
                     <div class="social">
@@ -88,27 +77,25 @@
                         <a href="#" target="_blank"><i class='bx bxl-github'></i></a>
                     </div>
                 </div>
-                
             </div>
         </div>
-
         <div class="end">
             <p>Copyright © 2024 Pop Expeditions All Rights Reserved.<br>Website developed by: <a href="#">Abugahh and LancemDev</a></p>
         </div>
-        </section>
+    </section>
 
-        <script>
-            window.addEventListener('scroll', function() {
-                const nav = document.querySelector('nav');
-                if (window.scrollY > 50) {
-                    nav.classList.add('scrolled');
-                } else {
-                    nav.classList.remove('scrolled');
-                }
-            });
-        </script>
+    <script>
+        window.addEventListener('scroll', function() {
+            const nav = document.querySelector('nav');
+            if (window.scrollY > 50) {
+                nav.classList.add('scrolled');
+            } else {
+                nav.classList.remove('scrolled');
+            }
+        });
+    </script>
 
     @livewireScripts
     <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
-    </body>
+</body>
 </html>
